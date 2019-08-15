0d66696000d89cbd574a127480e37c2794c9c952
Abhinav Kumar Sinha <abhiks3112@gmail.com>
2019-08-15 20:40:12 +0530

    A reference example of ball animation added

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,6 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: 'rallycoding',
+  rules:
+  {'react/require-extension': 'off'}
 };
--- src/Ball.js
+++ src/Ball.js
@@ -1,2 +1,35 @@
-import React, {Component} from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import { View, StyleSheet, Animated } from 'react-native';
+
+class Ball extends Component {
+    constructor(props) {
+        super(props);
+        this.position = new Animated.ValueXY(0, 0);        
+    }
+
+    componentDidMount() {
+        Animated.spring(this.position, {
+            toValue: { x: 200, y: 500 }
+        }).start();
+    }
+
+    render() {
+        return (
+        <Animated.View style={this.position.getLayout()}>
+            <View style={styles.ball} />
+        </Animated.View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    ball: {
+        height: 60,
+        width: 60,
+        borderColor: 'black',
+        borderRadius: 100,
+        borderWidth: 30
+    }
+});
+
+export default Ball;
