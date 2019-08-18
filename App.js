b7803d2ca6e367aa633f837fad48b09dbd11948e
Abhinav Kumar Sinha <abhiks3112@gmail.com>
2019-08-18 20:47:24 +0530

    Added Animation for swiping card left/right rotate

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow
  */
 
-import React from 'react';
+import React, { Component } from 'react';
 import {
   StyleSheet,
   View,
@@ -17,20 +17,19 @@ import { Card, Button } from 'react-native-elements';
 import Deck from './src/components/Deck';
 
 const DATA = [
-  { id: 1, text: 'Card #1', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+ /*  { id: 1, text: 'Card #1', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
   { id: 2, text: 'Card #2', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
   { id: 3, text: 'Card #3', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
   { id: 4, text: 'Card #4', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
   { id: 5, text: 'Card #5', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
   { id: 6, text: 'Card #6', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
-  { id: 7, text: 'Card #7', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+  { id: 7, text: 'Card #7', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' }, */
   { id: 8, text: 'Card #8', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
 ];
 
 
-const App = () => {
-  renderCard = (item) => {
-    return (
+class App extends Component {
+  renderCard = (item) => (
       <Card
         key={item.id} 
         title={item.text}
@@ -45,18 +44,32 @@ const App = () => {
           title='View Now'
         />
       </Card>
-    );
-  };
+    )
 
-  return (
-    <View style={styles.container}>
-      <Deck 
-        data={DATA}
-        renderCard={this.renderCard}
-      />
-    </View>
-  );
-};
+  renderNoMoreCards = () => (
+      <Card title='That is all, Folks!!'>
+        <Text style={{ marginBottom: 10 }} >
+          There's no more content here!!
+        </Text>
+        <Button 
+          backgroundColor='#03A9F4'
+          title='Get More'
+        />
+      </Card>
+    )
+
+  render() {
+      return (
+      <View style={styles.container}>
+        <Deck 
+          data={DATA}
+          renderCard={this.renderCard}
+          renderNoMoreCards={this.renderNoMoreCards}
+        />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
--- src/components/Deck.js
+++ src/components/Deck.js
@@ -8,10 +8,18 @@ import {
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 100;
 
 class Deck extends Component {
+    static defaultProps = {
+        onSwipeRight: () => {},
+        onSwipeLeft: () => {}
+    }
     constructor(props) {
         super(props);
+        this.state = {
+            index: 0
+        };
         this._position = new Animated.ValueXY();      
         this._panResponder = PanResponder.create({
             onStartShouldSetPanResponder: () => true,
@@ -20,15 +28,24 @@ class Deck extends Component {
             },
             onPanResponderRelease: (event, gesture) => {
                 if (gesture.dx > SWIPE_THRESHOLD) {
-                  console.log('swipe right');
+                   this.forceSwipe('right');                 
                 } else if (gesture.dx < -SWIPE_THRESHOLD) {
-                    console.log('swipe left');
-                } else {
-                    this.resetPosition();
+                   this.forceSwipe('left');                   
+                } else {                  
+                   this.resetPosition();
                 }
             }
         });
     }  
+
+    onSwipeComplete = (direction) => {
+        const { onSwipeLeft, onSwipeRight, data } = this.props;
+        const item = data[this.state.index];
+        // eslint-disable-next-line no-unused-expressions
+        direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item);
+        this._position.setValue({ x: 0, y: 0 });
+        this.setState({ index: this.state.index + 1 });
+    }
 
     getCardStyle = () => {
         const { _position } = this;
@@ -42,15 +59,28 @@ class Deck extends Component {
         });
     }
 
-    resetPosition = () => {
-        return Animated.spring(this._position, {
+    resetPosition = () => Animated.spring(this._position, {
             toValue: { x: 0, y: 0 }
-        }).start();
+        }).start() 
+
+    forceSwipe = (direction) => {
+        const x = direction === 'right' ? SCREEN_WIDTH * 1.2 : -SCREEN_WIDTH * 1.2;
+        return Animated.timing(this._position, {
+            toValue: { x, y: 0 },
+            duration: SWIPE_OUT_DURATION
+        }).start(() => this.onSwipeComplete(direction));
     } 
 
     renderCard = () => {
-        return this.props.data.map((item, index) => {
-            if (index === 0) {
+        if (this.state.index >= this.props.data.length) {
+            return this.props.renderNoMoreCards();
+        }
+        return this.props.data.map((item, i) => {
+            if (i < this.state.index) { 
+                return null; 
+            }
+
+            if (i === this.state.index) {
               return (
                 <Animated.View
                 key={item.id}
